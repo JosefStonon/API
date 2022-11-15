2466f8aeee14182803f77ebf015e6a935ce3ead7
JosefStonon <josef.sartori@outlook.com>
2022-11-15 04:16:50 -0300

    preparacao para inicio do projeto

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(3001, () => {
+  console.log('server is runnig on http://localhost:3001')
+// eslint-disable-next-line linebreak-style
+});
