62cf7429ee7f72a89c36f21588741b0eb81df16a
JosefStonon <josef.sartori@outlook.com>
2022-11-18 00:40:35 -0300

    criação do banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
-
+import { router } from './router';
 
 
 mongoose.connect('mongodb://127.0.0.1:27017')
   .then(() => {console.log('conectado ao mongodb')
   const app = express();
-  const port = 3002;
+  const port = 3001;
+
+
+  app.use(express.json());
+  app.use(router);
 
     app.listen(port, () => {
       console.log(`server is runing on https://localhost:${port}`);
